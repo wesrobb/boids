a6f3775f849fe356586d92f1e1ff4a85f176fcda
Wesley Robb <wesrobb86@gmail.com>
2019-08-26 21:48:06 +0200

    Start playing with OpenGL

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,4 @@
+def Settings( **kwargs ):
+  return {
+    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-I', 'lib/SDL2-2.0.10/include' ],
+  }
---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include "SDL_opengl_glext.h"
+#include <stdio.h>
+
+typedef void (GLAPIENTRY *glClearColorFn)(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
+
+typedef void (GLAPIENTRY *glClearFn)(GLbitfield mask);
+
+int main(int argc, char* argv[])
+{
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+    SDL_Init(SDL_INIT_EVERYTHING);         // Initialize SDL2
+
+    // Create an application window with the following settings:
+    SDL_Window *window = SDL_CreateWindow(
+        "An SDL2 window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL
+    );
+
+    // Check that the window was successfully created
+    if (window == 0)
+    {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_GLContext gl = SDL_GL_CreateContext(window);
+
+
+    // TODO: Ensure these are not null
+    glClearColorFn glClearColor = (glClearColorFn)SDL_GL_GetProcAddress("glClearColor");
+    glClearFn glClear = (glClearFn)SDL_GL_GetProcAddress("glClear");
+
+    // The window is open could enter program loop here (see SDL_PollEvent())
+    bool stopping = false;
+    while (!stopping)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_KEYUP:
+                {
+                    if (event.key.keysym.sym == SDLK_ESCAPE)
+                    {
+                        stopping = true;
+                    }
+                }
+            }
+            /* handle your event here */
+        }
+        /* do some other stuff here -- draw your app, etc. */
+        glClearColor(1.0f, 0.5f, 0.0f, 10.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DeleteContext(gl);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+
